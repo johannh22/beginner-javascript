c7c8a06763e1b40d1909970f4643582b6cbfcec5
Johann <62679700+johannh22@users.noreply.github.com>
2022-04-11 10:17:52 -0300

    First module (types) done

---
+++ playground/types.js
@@ -0,0 +1,23 @@
+/* eslint-disable */
+// const firstName = 'wes';
+// const middle = "topher";
+// const last = `bos`;
+
+// const sentence = 'she\'s so "cool"';
+// const sentence2 = "she's so \"cool\"";
+// const sentence3 = `she's so "cool"`;
+
+// const hello = 'hello my name is ' + firstName + '. Nice to meet you';
+
+// let hello = 'hello my name is ';
+// hello = hello + firstName;
+// hello = hello + '. Nice to meet you';
+
+// const hello = `hello my name is ${firstName}. Nice to meet you`;
+
+/* const person = {
+  first: 'johann',
+  last: 'homonnai',
+  age: 22
+}; */
+
