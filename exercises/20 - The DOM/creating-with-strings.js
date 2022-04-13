b62a0a802912fc157d13cc2553884e5608919cec
Johann <62679700+johannh22@users.noreply.github.com>
2022-04-13 11:20:17 -0300

    DOM Section almost finished

---
+++ exercises/20 - The DOM/creating-with-strings.js
@@ -0,0 +1,16 @@
+const item = document.querySelector('.item');
+
+const src = `https://picsum.photos/200`;
+const desc = `Cute Pup <img onload="alert('HACKED')" src="https://picsum.photos/50"/>`;
+const myHTML = `
+  <div class="wrapper">
+    <h2>Cute ${desc}</h2>
+    <img src="${src}" alt="${desc}"/>
+  </div>
+`;
+
+// turn a string into a DOM element
+const myFragment = document.createRange().createContextualFragment(myHTML);
+
+console.log(myFragment.querySelector('img'));
+console.log(myFragment);
---
+++ exercises/20 - The DOM/traversing.js
@@ -0,0 +1,20 @@
+// const jojo = document.querySelector('.jojo');
+
+// Elements
+// console.log(jojo.children);
+// console.log(jojo.firstElementChild);
+// console.log(jojo.lastElementChild);
+// console.log(jojo.previousElementSibling);
+// console.log(jojo.nextElementSibling);
+// console.log(jojo.parentElement);
+
+// Nodes
+// console.log(jojo.childNodes);
+// console.log(jojo.firstChild);
+// console.log(jojo.previousSibling);
+// console.log(jojo.nextSibling);
+// console.log(jojo.parentNode);
+
+const p = document.createElement('p');
+p.textContent = 'I will be removed';
+document.body.append(p);
