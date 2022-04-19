37ca243817a6c8cf0a04eb967b1aa92e7b831566
Johann <62679700+johannh22@users.noreply.github.com>
2022-04-19 00:07:51 -0300

    `events` files finished

--- exercises/29 - Events/events.js
+++ exercises/29 - Events/events.js
@@ -1,5 +1,4 @@
 const butts = document.querySelector('.butts');
-const buyButtons = document.querySelectorAll('.buy');
 
 function handleClick() {
   console.log('IT GOT CLICKED!!!!!!!');
@@ -7,7 +6,38 @@ function handleClick() {
 
 butts.addEventListener('click', handleClick);
 
-buyButtons.forEach((button) => {
-  console.log('Binding the buy button');
-  button.addEventListener('click', handleClick);
+butts.removeEventListener('click', handleClick);
+
+const buyButtons = document.querySelectorAll('.buy');
+
+function handleBuyButtonClick(e) {
+  console.log('You clicked a button');
+  const button = e.target;
+  console.log(button.textContent);
+  console.log(parseFloat(e.target.dataset.price));
+  console.log(`target:`, e.target);
+  console.log(`currentTarget:`, e.currentTarget);
+  console.log(e.target === e.currentTarget);
+  // stop this event from bubbling up
+  e.stopPropagation();
+}
+
+buyButtons.forEach((buyButton) => {
+  buyButton.addEventListener('click', handleBuyButtonClick);
+});
+
+window.addEventListener(
+  'click',
+  (event) => {
+    console.log('YOU CLICKED THE WINDOW');
+    console.log(event.target);
+  },
+  { capture: true }
+);
+
+const photoEl = document.querySelector('.photo');
+
+photoEl.addEventListener('mouseenter', (e) => {
+  console.count(e.currentTarget);
+  console.log(this);
 });
