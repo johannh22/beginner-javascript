16719bf22d6a30c96a155441b8e855b41836a5db
Johann <62679700+johannh22@users.noreply.github.com>
2022-04-19 15:40:56 -0300

    Finished etch-a-sketch

--- exercises/33 - Etch-a-Sketch/etch-a-sketch.js
+++ exercises/33 - Etch-a-Sketch/etch-a-sketch.js
@@ -3,7 +3,7 @@ const canvas = document.querySelector('#etch-a-sketch');
 const ctx = canvas.getContext('2d');
 
 const shakebutton = document.querySelector('.shake');
-const MOVE_AMOUNT = 20;
+const MOVE_AMOUNT = 25;
 
 // setup our canvas for drawing
 // make a variable called height and width from the same properties on our canvas
@@ -11,12 +11,14 @@ const { width, height } = canvas;
 
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
-ctx.lineWidth = 15;
+ctx.lineWidth = 50;
 
 // create random x and y starting points on the canvas
 let x = Math.floor(Math.random() * width);
 let y = Math.floor(Math.random() * height);
 
+let hue = 0;
+ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
 ctx.beginPath(); // start the drawing
 ctx.moveTo(x, y);
 ctx.lineTo(x + 0.0001, y + 0.0001);
@@ -24,9 +26,10 @@ ctx.stroke();
 
 // write a draw function
 function draw({ key }) {
-  console.log(key);
   ctx.beginPath();
   ctx.moveTo(x, y);
+  hue += 10;
+  ctx.strokeStyle = `hsl(${Math.random() * 360}, 100%, 50%)`;
   // move our x and y values depending on what the user did
   switch (key) {
     case 'ArrowUp':
@@ -58,6 +61,20 @@ function handleKey(e) {
 }
 
 // clear / shake function
+function clearCanvas() {
+  canvas.classList.add('shake');
+  ctx.clearRect(0, 0, width, height);
+  canvas.addEventListener(
+    'animationend',
+    () => {
+      canvas.classList.remove('shake');
+    },
+    { once: true }
+  );
+}
 
 // listen for arrow keys
 window.addEventListener('keydown', handleKey);
+
+// listen for button click
+shakebutton.addEventListener('click', clearCanvas);
