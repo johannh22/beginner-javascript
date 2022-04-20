56735cc283658878ce7829c36d3e5c49c47421cc
Johann <62679700+johannh22@users.noreply.github.com>
2022-04-20 14:28:36 -0300

    Finished scroll observer

--- exercises/35 - Scroll To Accept/scroll-to-accept.js
+++ exercises/35 - Scroll To Accept/scroll-to-accept.js
@@ -1,9 +1,16 @@
 const terms = document.querySelector('.terms-and-conditions');
-const watch = document.querySelector('.watch');
+const button = document.querySelector('button.accept');
 
 function obCallback(payload) {
-  console.log(payload[0]);
+  if (payload[0].intersectionRatio === 1) {
+    button.disabled = false;
+    // stop observing the button
+    ob.unobserve(terms.lastElementChild);
+  }
 }
-const ob = new IntersectionObserver(obCallback);
+const ob = new IntersectionObserver(obCallback, {
+  root: terms,
+  threshold: 1,
+});
 
-ob.observe(watch);
+ob.observe(terms.lastElementChild);
